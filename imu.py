7e182f391b164fc0ae26ca224ffd542d0f4053ca
kedus42 <k.mathewos@jacobs-university.de>
2021-01-10 14:08:55 +0100

    Added linear acceleration reading

--- imu.py
+++ imu.py
@@ -3,6 +3,9 @@ import time
 
 bno = BNO055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
 bno.begin()
+xpos=0
+ypos=0
+zpos=0
 while True:
     sys, gyro, acc, mag = bno.get_calibration_status()
     print("sys calibration:", sys)
@@ -10,7 +13,13 @@ while True:
     print("acceleration calibration:", acc)
     print("magnetometer calibration:", mag)
     yaw, roll, pitch = bno.read_euler()
+    zacc, xacc, yacc = bno.linear_acceleration()
+    xpos+=xacc*xacc
+    ypos+=yacc*yacc
+    zpos+=zacc*zacc
     print("\n\nLatest readings:")
-    #print("Linear acceleration (m/s^2): {}".format(bno.linear_acceleration))
+    print("Linear acceleration:", xacc, yacc, zacc)
     print("rpy:", roll, pitch, yaw)
+    print("xyz position:", xpos, ypos, zpos)
+    calibration_values=bno.get_calibration()
     time.sleep(1)
