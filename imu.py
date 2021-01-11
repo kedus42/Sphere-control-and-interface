ec3beda1717d60ddf25248ec02a466bcc0561d3e
kedus42 <k.mathewos@jacobs-university.de>
2021-01-11 13:39:41 +0100

    Linear acceleration readings added

--- imu.py
+++ imu.py
@@ -6,20 +6,28 @@ bno.begin()
 xpos=0
 ypos=0
 zpos=0
+calibration_values=[244, 255, 15, 0, 238, 255, 213, 0, 168, 254, 161, 252, 0, 0, 253, 255, 0, 0, 232, 3, 114, 3]
+bno.set_calibration(calibration_values)
 while True:
     sys, gyro, acc, mag = bno.get_calibration_status()
     print("sys calibration:", sys)
     print("gyro calibration:", gyro)
     print("acceleration calibration:", acc)
     print("magnetometer calibration:", mag)
+    print(calibration_values)
     yaw, roll, pitch = bno.read_euler()
-    zacc, xacc, yacc = bno.linear_acceleration()
-    xpos+=xacc*xacc
-    ypos+=yacc*yacc
-    zpos+=zacc*zacc
+    xacc, yacc, zacc = bno.read_linear_acceleration()
     print("\n\nLatest readings:")
     print("Linear acceleration:", xacc, yacc, zacc)
+    if xacc<0.2 and xacc>-0.2:
+        xacc=0
+    if yacc<0.2 and yacc>-0.2:
+        yacc=0
+    if zacc<0.2 and zacc>-0.2:
+        zacc=0
+    xpos+=xacc*.1*.1
+    ypos+=yacc*.1*.1
+    zpos+=zacc*.1*.1
     print("rpy:", roll, pitch, yaw)
     print("xyz position:", xpos, ypos, zpos)
-    calibration_values=bno.get_calibration()
-    time.sleep(1)
+    time.sleep(.1)
