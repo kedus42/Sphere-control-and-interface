9999289b17a440f269c477468a39058c201f2c57
kedus42 <k.mathewos@jacobs-university.de>
2021-04-08 13:24:42 +0200

    clean up

--- src/utils/cam_server.py
+++ src/utils/cam_server.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2.7
 import rospy
 import picamera
-import time
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
@@ -10,11 +9,10 @@ bridge = CvBridge()
 camera_pub=rospy.Publisher('cam', Image, queue_size=30)
 camera=picamera.PiCamera()
 camera.vflip=True
+rate=Rate(float(1/30))
 while True:
-    start=time.time()
     camera.capture('latest.jpg')
     image_message = cv2.imread('latest.jpg')
     cv_image = bridge.cv2_to_imgmsg(image_message, "bgr8")
     camera_pub.publish(cv_image)
-    end=time.time()
-    time.sleep(1/30-(start-end))
+    rate.sleep()
