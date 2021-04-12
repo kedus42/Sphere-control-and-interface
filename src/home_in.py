20d4197f84b85d893e8d250ce544995f86d5e11e
kedus42 <k.mathewos@jacobs-university.de>
2021-04-12 17:37:09 +0200

    Detections fixed

--- src/home_in.py
+++ src/home_in.py
@@ -6,23 +6,27 @@ import numpy as np
 from sphere_control.msg import drive_msg
 import picamera
 
-noois=cv2.CascadeClassifier("../../haarcascades/haarcascade_lowerbody.xml")
+noois=cv2.CascadeClassifier("home/pi/ros_catkin_ws/src/sphere_control/haarcascades/haarcascade_lowerbody.xml")
 
 rospy.init_node('home_in')
 camwidth=1920
 camheight=1080
-move_threshold=int(camwidth*.8)
+upper_threshold=int(camwidth*.8)
+lower_threshold=int(camwidth*.1)
 steer_dist=25
 
 #bridge=CvBridge()
 steering_pub=rospy.Publisher('controller', drive_msg, queue_size=3)
 
 camera=picamera.PiCamera()
+camera.resolution=(1280*720)
 camera.vflip=True
 camera.hflip=True
 fps=2.0
+greatestw=0
 
 def callback(image):
+    global greatestw
     command=drive_msg()
     camera.capture('latest.jpg')
     img = cv2.imread('latest.jpg')
@@ -30,19 +34,18 @@ def callback(image):
     count=0
     for x,y,w,h in oois:
         cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 1)
-        if w<move_threshold:
+        if w<upper_threshold and w>lower_threshold:
             command.dir=1
             command.steer_dist=steer_dist
             command.duty_cycle=30
-            if x+w/2 < int((camwidth/2)-camwidth/10):
-                command.steer=-1
-            elif x+w/2 > int((camwidth/2)+camwidth/10):
-                command.steer=1
-            else:
-                command.steer=0
-        count+=1
-        if count==1:
-            break
+            if w>greatestw:
+                greatesw=w
+                if x+w/2 < int((camwidth/2)-camwidth/10):
+                    command.steer=-1
+                elif x+w/2 > int((camwidth/2)+camwidth/10):
+                    command.steer=1
+                else:
+                    command.steer=0
     cv2.imshow("sphere cam at "+ str(fps)+" fps", img)
     cv2.imwrite("detected imgs.jpg", img)
     steering_pub.publish(command)
